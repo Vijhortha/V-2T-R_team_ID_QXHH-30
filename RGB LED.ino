31057187cee96eb2e34d2863c5381f317fde2f7b
vijhortha <vijho2211@gmail.com>
2026-03-28 13:55:13 +0530

    Simple code for only RGB LED

---
+++ RGB LED.ino
@@ -0,0 +1,36 @@
+const int RED_PIN = 9;
+const int GREEN_PIN = 10;
+const int BLUE_PIN = 11;
+
+void setup()
+{
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+}
+
+void loop() {
+    // Red (turn just the red LED on):
+
+  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, LOW);
+
+  delay(1000);
+
+  // Green (turn just the green LED on):
+
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(GREEN_PIN, HIGH);
+  digitalWrite(BLUE_PIN, LOW);
+
+  delay(1000);
+
+  // Blue (turn just the blue LED on):
+
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, HIGH);
+
+  delay(1000);
+}
