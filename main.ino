aa98f3bddaace6ae09aa4ee196ecec6e88b1eb62
vijhortha <vijho2211@gmail.com>
2026-03-28 17:01:55 +0530

    main program with correct pin numbers and logic

--- main.ino
+++ main.ino
@@ -1,13 +1,21 @@
 // IR Sensor Pins
 int leftIR = 2;
-int centerIR = 3;
-int rightIR = 4;
+int centerIR = 12;
+int rightIR = 3;
 
 // Motor Driver Pins
-int IN1 = A0;
-int IN2 = A1;
-int IN3 = A2;
-int IN4 = A3;
+int IN1 = 6;
+int IN2 = 7;
+int IN3 = 8;
+int IN4 = 11;
+
+// Enable Pins (PWM)
+int ENA = 9;
+int ENB = 10;
+
+// Speed values (0–255)
+int speedForward = 150;
+int speedTurn = 120;
 
 void setup() {
   pinMode(leftIR, INPUT);
@@ -18,6 +26,9 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 }
 
 void loop() {
@@ -25,35 +36,43 @@ void loop() {
   int center = digitalRead(centerIR);
   int right = digitalRead(rightIR);
 
-  // Move Forward (on line)
-  if (left == HIGH && center == LOW && right == HIGH) {
+  // Move Forward
+  if (left == HIGH && center == HIGH && right == HIGH) {
     forward();
   }
 
   // Turn Right
-  else if (left == HIGH && center == HIGH && right == LOW) {
+  else if (left == HIGH && center == LOW && right == LOW) {
     rightTurn();
   }
 
   // Turn Left
-  else if (left == LOW && center == HIGH && right == HIGH) {
+  else if (left == LOW && center == LOW && right == HIGH) {
     leftTurn();
   }
 
-  // All sensors on black (junction or thick line)
-  else if (left == LOW && center == LOW && right == LOW) {
+  // Thick line / junction
+  else if (left == LOW && center == HIGH && right == LOW) {
     forward();
   }
 
-  // Lost line
+  // Reverse condition
+  else if (left == HIGH && center == LOW && right == HIGH) {
+    reverse();
+  }
+
+  // Stop (lost line)
   else {
     stopMotors();
   }
 }
 
-// FUNCTIONS
+// ================= FUNCTIONS =================
 
 void forward() {
+  analogWrite(ENA, speedForward);
+  analogWrite(ENB, speedForward);
+
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
@@ -61,6 +80,9 @@ void forward() {
 }
 
 void leftTurn() {
+  analogWrite(ENA, speedTurn);
+  analogWrite(ENB, speedForward);
+
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
@@ -68,13 +90,29 @@ void leftTurn() {
 }
 
 void rightTurn() {
+  analogWrite(ENA, speedForward);
+  analogWrite(ENB, speedTurn);
+
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 }
 
+void reverse() {
+  analogWrite(ENA, speedForward);
+  analogWrite(ENB, speedForward);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
 void stopMotors() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
