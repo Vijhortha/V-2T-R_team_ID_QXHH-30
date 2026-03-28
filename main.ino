e6632afad6e3e155fe4128c1cc078c98741715ed
Taksha Tangudu <145993592+T-Taksha@users.noreply.github.com>
2026-03-28 14:24:58 +0530

    Update ultrasonic sensor function in main.ino
    Changed pin numbers and Added ultrasonic sensor pins

--- main.ino
+++ main.ino
@@ -1,29 +1,43 @@
 // ----------- SENSOR CONNECTIONS -------------
-#define leftSensor  34   // LEFT IR Sensor OUT
-#define rightSensor 35   // RIGHT IR Sensor OUT
+#define leftSensor  2   // LEFT IR Sensor OUT
+#define rightSensor 3   // RIGHT IR Sensor OUT
+
+// ----------- ULTRASONIC ---------------------
+#define trigPin 7
+#define echoPin 6
 
 // ----------- MOTOR CONNECTIONS -------------
 // LEFT MOTOR (connected to OUT3 & OUT4)
-#define IN3  14    // L298N IN3 → ESP32 D14 (Left motor direction 1)
-#define IN4  12    // L298N IN4 → ESP32 D12 (Left motor direction 2)
+#define IN3  10    // L298N IN3 → ESP32 D14 (Left motor direction 1)
+#define IN4  11    // L298N IN4 → ESP32 D12 (Left motor direction 2)
 
 // RIGHT MOTOR (connected to OUT1 & OUT2)
-#define IN1  26    // L298N IN1 → ESP32 D26 (Right motor direction 1)
-#define IN2  27    // L298N IN2 → ESP32 D27 (Right motor direction 2)
+#define IN1  8    // L298N IN1 → ESP32 D26 (Right motor direction 1)
+#define IN2  9    // L298N IN2 → ESP32 D27 (Right motor direction 2)
 
-#define ENA  33
-#define ENB  32 
+#define ENA  5
+#define ENB  3
+
+int urgentCount = 0;
+int normalCount = 0;
+
 void setup() {
 
   pinMode(leftSensor, INPUT);
   pinMode(rightSensor, INPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+
   pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -31,6 +45,21 @@ void loop() {
   int leftStatus  = digitalRead(leftSensor);
   int rightStatus = digitalRead(rightSensor);
 
+  float distance = getDistance();
+
+  // -------- REQUEST DETECTION --------
+  if (distance > 0 && distance < 10) {
+    urgentCount++;
+    Serial.println("URGENT REQUEST");
+    delay(500);
+  }
+  else if (distance >= 10 && distance < 20) {
+    normalCount++;
+    Serial.println("NORMAL REQUEST");
+    delay(500);
+  }
+
+  // -------- LINE FOLLOWING --------
   if (leftStatus == HIGH && rightStatus == HIGH) {
     moveForward();
   }
@@ -45,39 +74,55 @@ void loop() {
   }
 }
 
-// ----------- MOVEMENT FUNCTIONS ----------------
 
+// ----------- ULTRASONIC FUNCTION ------------
+float getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 20000);
+  float distance = duration * 0.034 / 2;
+
+  return distance;
+}
 void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, 45);
+  analogWrite(ENA, 120);
+
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENB, 45);
+  analogWrite(ENB, 120);
 }
 
 void turnLeft() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, 60);
+  analogWrite(ENA, 130);
+
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENB, 50);
+  analogWrite(ENB, 110);
 }
+
 void turnRight() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  analogWrite(ENA, 65);
+  analogWrite(ENA, 110);
+
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENB, 70);
+  analogWrite(ENB, 130);
 }
 
 void stopMotors() {
   digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(ENA, 60);
+  digitalWrite(IN2, LOW);
+
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENB, 40);
+  digitalWrite(IN4, LOW);
 }
